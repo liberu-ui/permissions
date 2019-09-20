7c3d1b54c8d7b0a031c36202bcd212e30a44e622
reza <raftx24@gmail.com>
2019-09-21 00:36:45 +0430

    first commit

---
+++ src/bulma/routes/system/permissions/index.js
@@ -0,0 +1,11 @@
+const PermissionsIndex = () => import('../../../pages/permissions/Index.vue');
+
+export default {
+    name: 'system.permissions.index',
+    path: '',
+    component: PermissionsIndex,
+    meta: {
+        breadcrumb: 'index',
+        title: 'Permissions',
+    },
+};
