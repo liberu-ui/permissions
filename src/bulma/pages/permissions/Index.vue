782217d8db6cd05d7cfb2fbb4642de1e95211b3b
mishu <mishu@xtelecom.ro>
2020-03-14 17:44:40 +0200

    refines permission status to a computed property

--- src/bulma/pages/permissions/Index.vue
+++ src/bulma/pages/permissions/Index.vue
@@ -3,7 +3,7 @@
         id="permissions">
         <template v-slot:type="{ row }">
             <span class="tag is-table-tag"
-                :class="row.isRead ? 'is-success' : 'is-warning'">
+                :class="cssClass(row)">
                 {{ row.type }}
             </span>
         </template>
@@ -11,11 +11,33 @@
 </template>
 
 <script>
+import { mapState } from 'vuex';
 import { EnsoTable } from '@enso-ui/tables/bulma';
 
 export default {
     name: 'Index',
 
     components: { EnsoTable },
+
+    computed: {
+        ...mapState(['enums']),
+    },
+
+    methods: {
+        cssClass({ type }) {
+            switch (type) {
+            case this.enums.permissionTypes.Read:
+                return 'is-success';
+            case this.enums.permissionTypes.Write:
+                return 'is-warning';
+            case this.enums.permissionTypes.Delete:
+                return 'is-danger';
+            case this.enums.permissionTypes.Menu:
+                return 'is-info';
+            default:
+                return '';
+            }
+        },
+    },
 };
 </script>
